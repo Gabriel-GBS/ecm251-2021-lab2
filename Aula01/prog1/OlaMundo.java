5bb6f5a6bf0ad5ff524d30fea66b3b65ecfc39cc
GabrielGomesbs <79426902+GabrielGomesbs@users.noreply.github.com>
2021-02-25 09:21:29 -0300

    Alteração na base

--- Aula01/prog1/OlaMundo.java
+++ Aula01/prog1/OlaMundo.java
@@ -1,5 +1,8 @@
 class OlaMundo{
     public static void main(String[] args) {
-       System.out.println("Ola Mundo!"); 
+       System.out.println("Ola Mundo"); 
+       System.out.println("Stein é mal?");
+       System.out.println("Overwatch morreu?");
+       System.out.println("Aqui!");
     }
 }
