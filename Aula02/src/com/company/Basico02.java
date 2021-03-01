af2f0aaf438e251e75b499458bc652e427dc99c0
GabrielGomesbs <79426902+GabrielGomesbs@users.noreply.github.com>
2021-03-01 11:22:22 -0300

    Aula 2 atualização

---
+++ Aula02/src/com/company/Basico02.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Basico02 {
+    //Estruturas decisão
+    public static void main(String[] args) {
+        int ano = 2021;
+        // if (ano % 2 == 0){
+        //System.out.println("Ano Par!");
+        //}
+        //if (ano % 2 != 0){
+        //System.out.println("Ano Impar!");
+        //}
+        //Estrutura de decisão composta
+        if (ano % 2 == 0){
+            System.out.println("Ano Par!");
+    }else{
+            System.out.println("Ano Impar!");
+        }
+
+    }
+}
+
