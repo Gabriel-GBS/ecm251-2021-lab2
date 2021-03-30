4f7d30a6aceed2bd8562af6ad343aa493510e6aa
GabrielGomesbs <79426902+Gabriel-GBS@users.noreply.github.com>
2021-03-30 14:46:55 -0300

    Inicio Atividade 1

---
+++ Atividade1/src/gomes/gabriel/Main.java
@@ -0,0 +1,13 @@
+// Gabriel Gomes - RA: 18.00947-6
+//
+
+package gomes.gabriel;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Sistema sistema = new Sistema();
+        sistema.executar();
+
+    }
+}
